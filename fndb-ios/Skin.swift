505a1f034181a2ef73300528d75fddd925976f87
fmcfar200 <fmcfar200@caledonian.ac.uk>
2018-10-18 10:49:34 +0100

    Skin Object Reading

---
+++ fndb-ios/Skin.swift
@@ -0,0 +1,43 @@
+//
+//  Skin.swift
+//  fndb-ios
+//
+//  Created by fraser mcfarlane on 18/10/2018.
+//  Copyright © 2018 P-Flow Studios. All rights reserved.
+//
+
+class Skin
+{
+    var id:String?
+    var name:String?
+    var rarity:String?
+    var desc:String?
+    var cost:String?
+    var imageLinkSmall:String?
+    
+    init(id:String, name:String, rarity:String, desc:String, imageLinkSmall:String)
+    {
+        self.id = id
+        self.name = name
+        self.rarity = rarity
+        self.desc = desc
+        self.imageLinkSmall = imageLinkSmall
+        
+        switch rarity {
+        case "Uncommon":
+            self.cost = "800"
+        case "Rare":
+            self.cost = "1200"
+        case "Epic":
+            self.cost = "1500"
+        case "Legendary":
+            self.cost = "2000"
+            
+        default:
+            self.cost = "1000"
+        }
+        
+    }
+    
+    
+}
--- fndb-ios/SkinViewController.swift
+++ fndb-ios/SkinViewController.swift
@@ -12,17 +12,30 @@ import FirebaseDatabase
 
 class SkinViewController: UIViewController {
     
+    var skinCollection = [Skin]()
     let bPSkinRef = Database.database().reference().child("SPSkin")
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        var s1Skin = bPSkinRef.child("SP_S1_Skins")
         // Do any additional setup after loading the view.
-        bPSkinRef.observeSingleEvent(of: .value, with: { (snapshot) in
-            // Get user value
-            debugPrint(snapshot.value)
+        s1Skin.observe(.value, with: { (snapshot) in
             
-            // ...
+            for item in snapshot.children.allObjects as! [DataSnapshot]
+            {
+                let SkinObject = item.value as? [String: AnyObject]
+                let id = SkinObject?["id"]
+                let name = SkinObject?["name"]
+                let rarity = SkinObject?["rarity"]
+                let desc = SkinObject?["dsc"]
+                let imageLinkSmall = SkinObject?["imageLinkSmall"]
+
+                let theSkin = Skin(id: id as! String, name: name as! String, rarity: rarity as! String, desc: desc as! String, imageLinkSmall: imageLinkSmall as! String)
+                self.skinCollection.append(theSkin)
+            }
+            
         }) { (error) in
             print(error.localizedDescription)
         }
