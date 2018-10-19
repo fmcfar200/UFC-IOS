1255f3d70ed678aa791ab6c9b22084dfeeac3e76
fmcfar200 <fmcfar200@caledonian.ac.uk>
2018-10-20 00:23:16 +0100

    News

--- fndb-ios/News.swift
+++ fndb-ios/News.swift
@@ -6,23 +6,23 @@
 //  Copyright © 2018 P-Flow Studios. All rights reserved.
 //
 
-class News {
+class News: Decodable {
     
     var title:String?
     var body:String?
-    var imageUrl:String?
-    var timeMillis:CLong?
+    var image:String?
+    var time:String?
     
-    init() {
-        
-    }
-    
-    init(title: String, body:String, imageUrl:String,timeMillis:CLong) {
-        self.title = title
-        self.body = body
-        self.imageUrl = imageUrl
-        self.timeMillis = timeMillis
-    }
+//    init() {
+//
+//    }
+//
+//    init(title: String, body:String, imageUrl:String,timeMillis:CLong) {
+//        self.title = title
+//        self.body = body
+//        self.imageUrl = imageUrl
+//        self.timeMillis = timeMillis
+//    }
     
 
 }
--- fndb-ios/NewsViewController.swift
+++ fndb-ios/NewsViewController.swift
@@ -8,6 +8,12 @@
 
 import UIKit
 
+struct NewsResponse: Decodable{
+    let type: String?
+    let typesm: String?
+    let entries: [News]
+}
+
 class NewsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     
@@ -21,6 +27,43 @@ class NewsViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         tableView.delegate = self
         tableView.dataSource = self
+        
+        let key = String("a4587bc8429ba5f7e2be4d869fddf5ff")
+        let url = URL(string: "https://fortnite-public-api.theapinetwork.com/prod09/br_motd/get")!
+        var request = URLRequest(url: url)
+        request.setValue(key, forHTTPHeaderField: "Authorization")
+        request.httpMethod = "POST"
+        var theCollection = [News]()
+        
+        DispatchQueue.global(qos: .userInteractive).async {
+            let task = URLSession.shared.dataTask(with: request) { data, response, error in
+                guard let data = data, error == nil else {                                                 // check for fundamental networking error
+                    print("error=\(String(describing: error))")
+                    return
+                }
+                
+                if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
+                    print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                    print("response = \(String(describing: response))")
+                }
+                
+                let responseString = String(data: data, encoding: .utf8)
+                //print("responseString = \(String(describing: responseString))")
+                do {
+                    let newsResponse = try JSONDecoder().decode(NewsResponse.self, from: data)
+                    self.newsCollection = newsResponse.entries
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                    }
+
+                    
+                }catch let jsonErr{
+                    print(jsonErr)
+                }
+            }
+            task.resume()
+        }
+        
 
         // Do any additional setup after loading the view.
     }
@@ -33,11 +76,16 @@ class NewsViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cellID = "NewsTableViewCell"
+        let cellID = "newsCell"
         guard let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath) as? NewsTableViewCell else{
             fatalError("cell error: not member of NewsTableViewCell")
         }
         
+        let news = self.newsCollection[indexPath.row]
+        cell.newsTitle.text = news.title
+        cell.newsBody.text = news.body
+        cell.newsBody.sizeToFit()
+        downloadImage(urlstr: news.image!, imageView: cell.newsImage!)
         
         cell.layoutSubviews()
         return cell
